f290896d8e2d99ebc73521ccbd6d3b009cdf3e33
unknown <michaelstanford@gmail.com>
2019-07-22 15:08:08 -0500

    initial commit

---
+++ install.ps1
@@ -0,0 +1,85 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+function Check-Command($cmdname) {
+    return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
+}
+
+# Rename Computer -----------------------------------------------------------------------------
+$computerName = Read-Host 'Enter New Computer Name'
+Write-Host "Renaming this computer to: " $computerName  -ForegroundColor Yellow
+Rename-Computer -NewName $computerName
+
+# Disable sleep when plugged in to power -----------------------------------------------------------------------------
+Write-Host ""
+Write-Host "Disable Sleep on AC Power..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+Powercfg /Change monitor-timeout-ac 20
+Powercfg /Change standby-timeout-ac 0
+
+# Enable WSL ------------------------------------------------------------------------------------
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+# Download Ubuntu ------------------------------------------------------------------------------------
+Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile Ubuntu.appx -UseBasicParsing
+
+# Install Ubuntu ------------------------------------------------------------------------------------
+Rename-Item ./Ubuntu.appx ./Ubuntu.zip
+Expand-Archive ./Ubuntu.zip ./Ubuntu
+
+
+# Enable developer mode -----------------------------------------------------------------------------
+
+Write-Host ""
+Write-Host "Enable Windows 10 Developer Mode..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
+
+
+# Enable Remote Desktop -----------------------------------------------------------------------------
+Write-Host ""
+Write-Host "Enable Remote Desktop..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\" -Name "fDenyTSConnections" -Value 0
+Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name "UserAuthentication" -Value 1
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+
+# -----------------------------------------------------------------------------
+
+if (Check-Command -cmdname 'choco') {
+    Write-Host "Choco is already installed, skip installation."
+}
+else {
+    Write-Host ""
+    Write-Host "Installing Chocolatey for Windows..." -ForegroundColor Green
+    Write-Host "------------------------------------" -ForegroundColor Green
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+Write-Host ""
+Write-Host "Installing Applications..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+
+if (Check-Command -cmdname 'git') {
+    Write-Host "Git is already installed, checking new version..."
+    choco update git -y
+}
+else {
+    Write-Host ""
+    Write-Host "Installing Git for Windows..." -ForegroundColor Green
+    choco install git -y
+}
+
+choco install 7zip.install -y
+choco install googlechrome -y
+choco install jetbrainstoolbox -y
+choco install launchy -y
+choco install slack -y
+choco install discord.install -y
+choco install docker-cli -y
+choco install visualstudio2019community -y --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
+
+
+
+Read-Host -Prompt "Setup finished, press [ENTER] to restart."
+Restart-Computer
